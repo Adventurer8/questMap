87bd7f33270802f793acf599612a56b660e1fc62
Adventurer8 <78566194+Adventurer8@users.noreply.github.com>
2021-10-15 23:10:15 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,18 @@ function setup () {
   	dt=5;
   	stroke(255,200);
   	strokeWeight(2);
+  	rectMode(CENTER);
+  	ellipseMode(CENTER);
+
+
+  	////	Карта	//////////////////////
+  	map = [];
+  	map[0] = new block(200,200, 300, 200);
+  	map[1] = new block(300,200, 500, 100);
+  	//////////////////////////////////////
 }
+
+let map;
 
 let t;
 let time;
@@ -26,7 +37,7 @@ let x,y;
 let dx,dy;
 let ddx, ddy;
 let dt;
-let R = 200;
+let R = 6;
 
 function draw () {
 
@@ -34,9 +45,13 @@ function draw () {
 
 	
 
-	fill(150);
+	fill(200, 220, 210);
 	noStroke();
-	rect(200,200,50,300);
+	// rect(200,200,50,300);
+
+	for(let i in map)
+		map[i].show();
+
 	// img.loadPixels();
 	// for (let x = 0; x < img.width; x++) {
  //    	for (let y = 0; y < img.height; y++) {
@@ -54,77 +69,66 @@ function draw () {
  	img.erase();
  	img.noStroke();
  	img.fill(255, 50);
- 	for(let i = 0; i<4; i++) {
- 		img.ellipse(mouseX, mouseY, (6-i)*50);
+ 	for(let i = 0; i<5; i++) {
+ 		img.ellipse(mouseX, mouseY, (R+sin(frameCount/20)/10-i)*50);
  	}
  	img.noErase();
 
- 	image(img, 0, 0);
+ 	if(random(400)>=5)	//	выспышки освещения
+ 		image(img, 0, 0);
 
 
 
- 	fill(127, 30, 15, 127*(1 + sin(frameCount/10)));
- 	ellipse(x,y,20);
-	/*if(keyCode == 17) return 0;
-
-	line(x, y, x+=dx, y+=dy);
-
-	d = dist(300,300,x,y);
-	dx+= d*d/100000 * ((600+time)*random() - x)/d;
-	dy+= d*d/100000 * (600*random() - y)/d;
+ 	stroke(0);
+ 	fill(250, 130, 15, 255*((frameCount %50)>25));
+ 	ellipse(x,y,20/*+10*sin(frameCount/10)/5*/);
 	
-	dx+= ddx/1000;
-	dy+= ddy/1000;
-
-	if(random()*10 > dt) {
-		dt = random()*10;
-		ddx = (random()*2 - 1)+0.5;
-		ddy = (random()*2 - 1);
-	}
-	if(dx+dy>8) {
-		dx*=0.9;
-		dy*=0.9;
-		ddx*=0.9;
-		ddy*=0.9;
-	}*/
-
 
 	if(keyIsDown(37)) {	// <
 		x--;
+		let bool = false;
+		for(let i in map) bool |= map[i].in(x, y);
+			print(bool);
+		if(!bool) x++;
 	}
 	if(keyIsDown(38)) {	// ^
 		y--;
+		let bool = false;
+		for(let i in map) bool |= map[i].in(x, y);
+		if(!bool) y++;
 	}
 	if(keyIsDown(39)) {	// >
 		x++;
+		let bool = false;
+		for(let i in map) bool |= map[i].in(x, y);
+		if(!bool) x--;
 	}
 	if(keyIsDown(40)) {	// v
 		y++;
+		let bool = false;
+		for(let i in map) bool |= map[i].in(x, y);
+		if(!bool) y--;
 	}
-	// if((x-100)**2 + (y-100)**2 < 100) R=300;
-	// ellipse(100,100,10);
+	
 
-	// print("ddx = ", round(ddx,3), "  ddy = ", round(ddy,3));
+}
 
+class block {
+	constructor(x, y, a, b) {
+		this.x = x;
+		this.y = y;
+		this.a = a;
+		this.b = b;
+	}
 
- //  	background(0);
-  	
- //  	ellipseMode(CENTER);
-	// for(let i=0;i<250;i++){
-	// 	noFill();
-	// 	stroke(250-t,255-i);
-	// 	ellipse(500,300,20+i+t,20+i+t);
-	// }
-	// fill(150,250,150);
-	// stroke(255);
-	// ellipse(500,300,20+255+10*sin(t/100),20+255+10*sin(t/100));
-	// t+=10;
-	// if(t>width/9*2){t=1;}
+	show() {
+		rect(this.x, this.y, this.a, this.b);
+	}
 
-
-	// stroke
-
- //  	image(img, 90, 80);
- //  	image(img, mouseX - img.width / 2, mouseY - img.height / 2);
-
+	in(x, y) {
+		let dx = (x - this.x) / this.a;
+		let dy = (y - this.y) / this.b;
+		if(abs(dx + dy) + abs(dx - dy) <= 1) return true
+			else return false;
+	}
 }
